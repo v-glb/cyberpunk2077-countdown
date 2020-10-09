68eb3b69bdedb800c55ae175be0d4afcf296bcf8
v-glb <v.networks@posteo.de>
2020-10-09 16:57:59 +0200

    Style ReleaseMsg

--- src/components/ReleaseMsg/ReleaseMsg.js
+++ src/components/ReleaseMsg/ReleaseMsg.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link, Logo } from 'arwes';
 
 export default function ReleaseMsg() {
   return (
-    <div>
-      Buy it now!!! 
+    <div className="ReleaseMsg">
+      <Logo animate size={200} />
+      <Link style={{ margin: '0px 20px 0px 20px'}} href='https://store.steampowered.com/app/1091500/Cyberpunk_2077/'>Buy it now!!!</Link>
+      <Logo animate size={200} />
     </div>
   )
 }
